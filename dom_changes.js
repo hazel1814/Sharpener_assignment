817cdeb6ad4feb9f6bee546bce44f9953f0ab8d9
hazel1814 <57265968+hazel1814@users.noreply.github.com>
2023-01-03 11:31:07 +0530

    Add files via upload

---
+++ dom_changes.js
@@ -0,0 +1,6 @@
+let output=document.getElementById('header-title')
+output.style.border='solid 3px black '
+
+let result=document.getElementById('item-title')
+result.style.fontWeight='bold'
+result.style.color="blue"
