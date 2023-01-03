94f3b0d7d498af6c9f6b2fbe37d1a3973f2e7274
hazel1814 <57265968+hazel1814@users.noreply.github.com>
2023-01-03 12:24:27 +0530

    Add files via upload

---
+++ tagname.js
@@ -0,0 +1,15 @@
+/*let output=document.getElementById('header-title')
+output.style.border='solid 3px black '
+
+let result=document.getElementById('item-title')
+result.style.fontWeight='bold'
+result.style.color="blue" 
+
+let third=document.querySelector('.list-group-item:ntH-child(3)')
+third.style.backgroundColor="green"
+
+let bold=document.querySelector('.list-group')
+bold.style.fontWeight="bold"*/
+
+var li=document.getElementsByTagName('li')
+console.log(li)
