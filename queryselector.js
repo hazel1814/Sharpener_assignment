2f260f0fde76739a36067bceda59decb31c33d55
hazel1814 <57265968+hazel1814@users.noreply.github.com>
2023-01-03 15:19:14 +0530

    Add files via upload

---
+++ queryselector.js
@@ -0,0 +1,5 @@
+let output=document.querySelector('.list-group-item:nth-child(2)')
+output.style.backgroundColor='green'
+
+let output1=document.querySelector('.list-group-item:nth-child(3)')
+output1.style.color='white'
