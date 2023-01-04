aff43894e1f420d337ad12a7aa78da75edd23283
hazel1814 <57265968+hazel1814@users.noreply.github.com>
2023-01-04 15:38:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+  var editBtn=document.createElement('button')
+
+  // Add classes to del button
+  editBtn.className= 'btn btn-danger btn-sm float-right delete ';
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+ 
+
+  // Append text node
+  editBtn.appendChild(document.createTextNode('Edit'));
+  deleteBtn.appendChild(document.createTextNode('X'));
+  
+
+  // Append button to li
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
+ 
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement; 
+      //if parent is not mentioned all are deleted with itemlist.remove  
+      //removechild doesnt work if there is no parent
+      itemList.removeChild(li);
+    }
+  }
+}
+
