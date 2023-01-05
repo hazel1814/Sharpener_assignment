6f1e7d27201a0ce2640a5cbe5ae6eb1a2d4b3480
hazel1814 <57265968+hazel1814@users.noreply.github.com>
2023-01-05 16:23:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
 
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
@@ -13,46 +17,70 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var description = document.getElementById('description').value;
+
 
   // Create new li element
   var li = document.createElement('li');
+
   // Add class
   li.className = 'list-group-item';
+
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
-
+  li.appendChild(document.createTextNode(description));
+ 
   // Create del button element
   var deleteBtn = document.createElement('button');
-  var editBtn=document.createElement('button')
+  var editbtn=document.createElement('button')
 
   // Add classes to del button
-  editBtn.className= 'btn btn-danger btn-sm float-right delete ';
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
- 
-
-  // Append text node
-  editBtn.appendChild(document.createTextNode('Edit'));
-  deleteBtn.appendChild(document.createTextNode('X'));
+  editbtn.className = 'btn btn btn-outline-warning btn-sm float-right delete';
   
 
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+  editbtn.appendChild(document.createTextNode('Edit'))
+
   // Append button to li
-  li.appendChild(editBtn);
   li.appendChild(deleteBtn);
- 
+  li.appendChild(editbtn)
 
   // Append li to list
   itemList.appendChild(li);
+  
 }
 
 // Remove item
 function removeItem(e){
   if(e.target.classList.contains('delete')){
     if(confirm('Are You Sure?')){
-      var li = e.target.parentElement; 
-      //if parent is not mentioned all are deleted with itemlist.remove  
-      //removechild doesnt work if there is no parent
+      var li = e.target.parentElement;
       itemList.removeChild(li);
     }
   }
 }
 
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    var description = item.childNodes[1].textContent;
+    
+    if(itemName.toLowerCase().indexOf(text) != -1||description.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+
+}
